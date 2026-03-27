266c9b001d3ec344800f16569bff5e9f544a30bc
Anupama Aluthge <anupamaaluthge@gmail.com>
2026-03-27 11:54:43 +0530

    Refactor client and server code to improve message handling and logging

--- client.py
+++ client.py
@@ -1,16 +1,38 @@
 import socket
+import threading
+from datetime import datetime
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 8888))
+client.connect(('127.0.0.1', 1112))
+
+time = datetime.now().strftime("%H:%M:%S")
+
+name = input("Enter your name: ")
+client.send(name.encode('utf-8'))
+
+def receive_message():
+    
+    while True:
+        try:
+            data = client.recv(1024)
+            if not data:
+                break
+            print(f"{time} server: {data.decode('utf-8')}")
+        except:
+            break
+
+def save_log(message):
+    with open("chat_log.txt", "a", encoding="utf-8") as f:
+        f.write(message + "\n")
+
+t = threading.Thread(target=receive_message)
+t.start()
 
 while True:
-    message = input('client: ')
-    client.send(message.encode('utf-8'))
-    
-    if message == 'exit':
+    msg = input("client: ")
+    if msg == "exit":
         break
+    client.send(msg.encode('utf-8'))
+    save_log(f"[{time}] client: {msg}")
     
-    response = client.recv(1024).decode('utf-8')
-    print(f'server {response}')
-
 client.close()
--- server.py
+++ server.py
@@ -1,8 +1,45 @@
 import socket
+import threading
+from datetime import datetime
 
-server = socket.socket(socket.AF_INET, socket.SOCK.STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+time = datetime.now().strftime("%H:%M:%S")
 
 # socket.AF_INET => we use IPv4 
 # socket.SOCK.STREAM => we use TCP 
 
-server.bind(('127.'))
+server.bind(('', 1112))
+
+server.listen(1) # max connections = 1
+print("server is ready! wait for client....")
+
+conn, addr = server.accept() # conn = socket object for client connection, addr = client address
+print(f'client connected: {addr}')
+
+user_name = conn.recv(1024).decode('utf-8')
+if not user_name:
+    user_name = "unknown"
+print(f"{user_name} is connected.")
+
+def recieve_message():
+    
+    while True:
+        try:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f'{time} {user_name}: {data.decode('utf-8')}')
+        except:
+            break
+t = threading.Thread(target=recieve_message)
+t.start()
+
+while True:
+    msg = input("server: ")
+    if msg == 'exit':
+        conn.send("Goodbye..!".encode('utf-8'))
+        break
+    conn.send(msg.encode('utf-8'))
+    
+conn.close()
+server.close()
